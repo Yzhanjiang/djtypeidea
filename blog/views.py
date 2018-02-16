30247e4d384a4aa813a5598ea8930206958c528c
zhan <251642766@qq.com>
2018-02-16 20:13:35 +0800

    添加template

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,21 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.http import  HttpResponse
 # Create your views here.
+
+def post_list(request,category_id=None,tag_id=None):
+
+    context = {
+        'name': 'typeidea'
+    }
+    return  render(request,'blog/list.html',context=context)
+
+
+def post_detail(request,post_id=None):
+    
+    
+    context = {
+        'name': 'typeidea'
+    }
+    return render(request,'blog/detail.html',context=context)
--- djtypeidea/settings.py
+++ djtypeidea/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'blog',
     'config',
     'comment',
+    'djtypeidea',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
