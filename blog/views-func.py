bd58eb98c1a6443756a6f3029316465e34874619
Yzhanjiang <251642766@qq.com>
2018-06-02 22:42:15 +0800

    修改Bug

--- blog/views-func.py
+++ blog/views-func.py
@@ -5,111 +5,14 @@ from django.shortcuts import render
 from django.http import  HttpResponse
 # Create your views here.
 
-from .models import Post,Tag,Category
+from config.models import Post,Tag,Category
 from config.models import SideBar
 from  comment.models import  Comment
+
 from django.http import  Http404
+
 from django.core.paginator import Paginator,EmptyPage
 
-from django.views.generic import ListView,DetailView
-
-
-####################class view
-class CommonMixin(object):
-    def get_category_context(self):
-        # context = super(CommonMixin, self).get_context_data()
-        categories = Category.objects.filter(status=1)
-        nav_cates = []
-        cates = []
-        for cate in categories:
-            if cate.is_nav:
-                nav_cates.append(cate)
-            else:
-                cates.append(cate)
-        return {
-            'nav_cates': nav_cates,
-            'cates': cates,
-        }
-
-    def get_side_bars(self):
-        # side_bars = SideBar.objects.filter(status=1)
-        return SideBar.objects.filter(status=1)
-
-
-    def get_context_data(self,**kwargs):
-
-        # side_bars = SideBar.objects.filter(status=1)
-        recently_posts = Post.objects.filter(status=1)[:10]
-        # hot_posts = Post.objects.filter(status=1).order_by('views')[:10]
-        recently_comments = Comment.objects.filter(status=1)[:10]
-
-        kwargs.update({
-            # 'side_bars': side_bars,
-            'recently_posts': recently_posts,
-            'recently_comments': recently_comments,
-        })
-        # context.update(extra_context)
-        # return context
-        return  super(CommonMixin,self).get_context_data(**kwargs)
-
-
-
-
-class BasePostsView(CommonMixin,ListView):
-    model = Post
-    template_name = 'blog/list.html'
-    context_object_name = 'posts'
-    paginate_by = 2
-
-class IndexView(BasePostsView):
-   pass
-
-class CategoryView(BasePostsView):
-    def get_queryset(self):
-        qs = super(CategoryView,self).get_queryset()
-        cate_id = self.kwargs.get('category_id')
-        qs = qs.filter(category_id = cate_id)
-        return  qs
-
-class TagView(BasePostsView):
-
-    def get_queryset(self):
-        tag_id = self.kwargs.get('tag_id')
-        try:
-            tag = Tag.objects.get(id=tag_id)
-        except Tag.DoesNotExist:
-            return []
-
-        posts = tag.posts.all()
-        return posts
-
-class PostView(CommonMixin,DetailView):
-    model = Post
-    template_name = 'blog/detail.html'
-    context_object_name = 'post'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##################################func
 def get_common_context():
     categories = Category.objects.filter(status=1)
     nav_cates = []
--- djtypeidea/urls.py
+++ djtypeidea/urls.py
@@ -19,16 +19,23 @@ from django.contrib import admin
 
 from .custom_site import custom_site
 
-from blog.views import post_list,post_detail
+from blog.views import post_list,post_detail,index
+from blog.views import IndexView,CategoryView,TagView,PostView
 from config.views import links
 # from  config.views import  links
 
 urlpatterns = [
+    #class view
+    url(r'^$',IndexView.as_view(),name="index"),
+    url(r'^tag/(?P<tag_id>\d+)/$',TagView.as_view(),name="tag"),
+    url(r'^category/(?P<category_id>\d+)/', CategoryView.as_view(), name="category"),
+    url(r'^post/(?P<pk>\d+)/$', PostView.as_view(), name="detail"),
 
-    url(r'^$',post_list,name="index"),
-    url(r'^category/(?P<category_id>\d+)/',post_list,name="category"),
-    url(r'^tag/(?P<tag_id>\d+)/$',post_list,name="tag"),
-    url(r'^post/(?P<pk>\d+)/$',post_detail,name="detail"),
+    #func
+    # url(r'^$',post_list,name="index"),
+    # url(r'^category/(?P<category_id>\d+)/',post_list,name="category"),
+    # url(r'^tag/(?P<tag_id>\d+)/$',post_list,name="tag"),
+    # url(r'^post/(?P<pk>\d+)/$',post_detail,name="detail"),
     url(r'links/$',links),
     url(r'^admin/', admin.site.urls),
     url(r'^cus_admin/', custom_site.urls),
