e7b7a637d26d443d6d2a911593562a7401a0491e
zhan <251642766@qq.com>
2018-06-10 00:21:14 +0800

    add  评论block

--- comment/forms.py
+++ comment/forms.py
@@ -6,10 +6,35 @@ from .models import Comment
 
 class CommentForm(forms.ModelForm):
     # target = forms.CharField(max_length=100,widget=forms.widgets.HiddenInput)
+
+    nickname = forms.CharField(
+        label='昵称',
+        max_length=50,
+        widget = forms.widgets.Input(
+            attrs={'class':'form-control','style':"width:60%"}
+        )
+    )
+
+    email = forms.CharField(
+        label='Email',
+        max_length=50,
+        widget=forms.widgets.Input(
+            attrs={'class': 'form-control','style':"width:60%"}
+        )
+    )
+    website = forms.CharField(
+        label='网站',
+        max_length=100,
+        widget=forms.widgets.URLInput(
+            attrs={'class': 'form-control','style':"width:60%"}
+        )
+    )
+
+
     content = forms.CharField(
         label="内容",
         max_length=500,
-        widget=forms.widgets.Textarea(attrs={'row':6,'clos':80})
+        widget=forms.widgets.Textarea(attrs={'row':6,'clos':60,'class':'form-control','style':"width:60%"})
     )
     def clean_content(self):
         content = self.cleaned_data.get('content')
--- config/views.py
+++ config/views.py
@@ -6,6 +6,8 @@ from .models import Link
 from django.views.generic import ListView
 from blog.views import CommonMixin
 from django.conf import  settings
+from comment.forms import CommentForm
+
 # Create your views here.
 
 class LinkView(CommonMixin,ListView):
@@ -16,7 +18,12 @@ class LinkView(CommonMixin,ListView):
     # paginate_by = 3
     # allow_empty = True
 
+    def get_context_data(self,**kwargs):
+        kwargs.update({
+            'comment_from':CommentForm(),
 
+        })
+        return super(ListView,self).get_context_data(**kwargs)
 
 
 def links(request):
